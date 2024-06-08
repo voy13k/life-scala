fb9b6b4d5d316ac2f05b2c967bf8b34254cf80a1
Voytek Murawski <voy13k@gmail.com>
2024-06-08 14:52:24 +1000

    Use consts to clarify neighboursOf behaviour

--- src/main/scala/Position.scala
+++ src/main/scala/Position.scala
@@ -2,15 +2,22 @@ case class Position(row: Long, col: Long) {
 
   override def toString: String = String.format("[%d, %d]", row, col)
 
+  private val ROW_ABOVE = row - 1
+  private val ROW_BELOW = row + 1
+  private val COL_LEFT = col - 1
+  private val COL_RIGHT = col + 1
+
   def neighboursOf: Seq[Position] = Seq(
-    Position(row - 1, col - 1),
-    Position(row - 1, col),
-    Position(row - 1, col + 1),
-    Position(row, col - 1),
-    Position(row, col + 1),
-    Position(row + 1, col - 1),
-    Position(row + 1, col),
-    Position(row + 1, col + 1),
+    Position(ROW_ABOVE, COL_LEFT),
+    Position(ROW_ABOVE, col),
+    Position(ROW_ABOVE, COL_RIGHT),
+
+    Position(row, COL_LEFT),
+    Position(row, COL_RIGHT),
+
+    Position(ROW_BELOW, COL_LEFT),
+    Position(ROW_BELOW, col),
+    Position(ROW_BELOW, COL_RIGHT),
   )
 
 }
