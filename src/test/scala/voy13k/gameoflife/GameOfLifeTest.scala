145b28a2a18cae841496900ba488237ff3cbc24c
Voytek Murawski <voy13k@gmail.com>
2024-06-21 23:17:21 +1000

    Rearrange testing context

--- src/test/scala/voy13k/gameoflife/GameOfLifeTest.scala
+++ src/test/scala/voy13k/gameoflife/GameOfLifeTest.scala
@@ -6,32 +6,28 @@ import org.scalatest.prop.TableDrivenPropertyChecks
 
 class GameOfLifeTest extends AnyFlatSpec with Matchers with TableDrivenPropertyChecks {
 
-  "A new game" should "be blank" in {
-    _givenNewGame._then.allCellsShouldBeDead
+  "A new game" should "be blank" in new GameContext {
+    _then.allCellsShouldBeDead
   }
 
-  "A seeded game" should "include all seeds" in {
-    _givenNewGame
-      ._when.cellsSpawned((2, 4), (7, 1))
-      ._then.onlyLiveCellsShouldBe((2, 4), (7, 1))
+  "A seeded game" should "include all seeds" in new GameContext {
+    _when.cellsSpawned((2, 4), (7, 1))
+    _then.onlyLiveCellsShouldBe((2, 4), (7, 1))
   }
 
-  "A json seeded game" should "include all seeds" in {
-    _givenNewGame
-      ._when.cellsSpawned(
-        """|[
-           |  [1, 3],
-           |  [1, 4],
-           |  [3, 5]
-           |]""".stripMargin)
-      ._then.onlyLiveCellsShouldBe((1, 4), (3, 5), (1, 3))
+  "A json seeded game" should "include all seeds" in new GameContext {
+    _when.cellsSpawned(
+      """|[
+         |  [1, 3],
+         |  [1, 4],
+         |  [3, 5]
+         |]""".stripMargin)
+    _then.onlyLiveCellsShouldBe((1, 4), (3, 5), (1, 3))
   }
 
-  "liveCellsJson" should "return valid json" in {
-    val testJson = "[[1,3],[4,6],[-1,2]]"
-    val fixture = _givenNewGame
-      ._with.liveCells((1, 3), (4, 6), (-1, 2))
-    fixture.game.liveCellsJson.replaceAll("\\s", "") should (
+  "liveCellsJson" should "return valid json" in new GameContext {
+    _with.liveCells((1, 3), (4, 6), (-1, 2))
+    game.liveCellsJson.replaceAll("\\s", "") should (
       equal("[[1,3],[4,6],[-1,2]]")
         or equal("[[1,3],[-1,2],[4,6]]")
         or equal("[[4,6],[1,3],[-1,2]]")
@@ -41,109 +37,110 @@ class GameOfLifeTest extends AnyFlatSpec with Matchers with TableDrivenPropertyC
       )
   }
 
-  "Repeated seed" should "be alive" in {
-    _givenNewGame
-      ._when.cellsSpawned(
-        """|[
-           |  [1, 3],
-           |  [1, 4],
-           |  [1, 3]
-           |]""".stripMargin)
-      ._then.onlyLiveCellsShouldBe((1, 4), (1, 3))
+  "Repeated seed" should "be alive" in new GameContext {
+    _when.cellsSpawned(
+      """|[
+         |  [1, 3],
+         |  [1, 4],
+         |  [1, 3]
+         |]""".stripMargin)
+    _then.onlyLiveCellsShouldBe((1, 4), (1, 3))
   }
 
-  "An invalid json seed" should "cause sensible exception" in {
+  "An invalid json seed" should "cause sensible exception" in new GameContext {
     an[IllegalArgumentException] should be thrownBy {
-      _givenNewGame._when.cellsSpawned("[[1]]")
+      _when.cellsSpawned("[[1]]")
     }
   }
 
-  "Seeding" should "be cumulative" in {
-    _givenNewGame
-      ._when.cellsSpawned((1, 3)).cellsSpawned((2, 5))
-      ._then.onlyLiveCellsShouldBe((1, 3), (2, 5))
+  "Seeding" should "be cumulative" in new GameContext {
+    _when.cellsSpawned((1, 3)).cellsSpawned((2, 5))
+    _then.onlyLiveCellsShouldBe((1, 3), (2, 5))
   }
 
-  "Killed cell" should "be dead" in {
-    _givenNewGame
-      ._with.liveCells((1, 4), (2, 3), (5, 9))
-      ._when.cellsKilled((2, 3), (1, 4))
-      ._then.onlyLiveCellsShouldBe((5, 9))
+  "Killed cell" should "be dead" in new GameContext {
+    _with.liveCells((1, 4), (2, 3), (5, 9))
+    _when.cellsKilled((2, 3), (1, 4))
+    _then.onlyLiveCellsShouldBe((5, 9))
   }
 
-  "Live cell with 0 neighbours" should "not survive single evolution" in {
-    val cell = (1, 5)
-    _givenNewGame
-      ._with.liveCells(cell)
-      ._when.evolved()
-      ._then.shouldBeDead(cell)
+  "Live cell with 0 neighbours" should "not survive single evolution" in new GameContext {
+    private val cell = (1, 5)
+    _with.liveCells(cell)
+    _when.evolved()
+    _then.shouldBeDead(cell)
   }
 
-  "Dead cell with 0 neighbours" should "be dead after evolution" in {
-    val cell = (1, 5)
-    _givenNewGame
-      ._with.deadCells(cell)
-      ._when.evolved()
-      ._then.shouldBeDead(cell)
+  "Dead cell with 0 neighbours" should "be dead after evolution" in new GameContext {
+    private val cell = (1, 5)
+    _with.deadCells(cell)
+    _when.evolved()
+    _then.shouldBeDead(cell)
   }
 
   "Live cell with 1 neighbour" should "not survive single evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 1)) { neighbours =>
-      _givenNewGame
-        ._with.liveCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeDead(cell)
+      new GameContext {
+        _with.liveCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeDead(cell)
+      }
     }
   }
 
   "Dead cell with 1 neighbour" should "be dead after evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 1)) { neighbours =>
-      _givenNewGame
-        ._with.deadCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeDead(cell)
+      new GameContext {
+        _with.deadCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeDead(cell)
+      }
     }
   }
 
   "Live cell with 2 neighbours" should "survive single evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 2)) { neighbours =>
-      _givenNewGame
-        ._with.liveCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeAlive(cell)
+      new GameContext {
+        _with.liveCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeAlive(cell)
+      }
     }
   }
 
   "Dead cell with 2 neighbours" should "be dead after single evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 2)) { neighbours =>
-      _givenNewGame
-        ._with.deadCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeDead(cell)
+      new GameContext {
+        _with.deadCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeDead(cell)
+      }
     }
   }
 
   "Live cell with 3 neighbours" should "survive single evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 3)) { neighbours =>
-      _givenNewGame
-        ._with.liveCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeAlive(cell)
+      new GameContext {
+        _with.liveCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeAlive(cell)
+      }
     }
   }
 
   "Dead cell with 3 neighbours" should "be alive after single evolution" in {
     val cell = (1, 5)
     forAll(neighbourCombinationsTable(cell, 3)) { neighbours =>
-      _givenNewGame
-        ._with.deadCellAndItsNeighbours(cell, neighbours)
-        ._when.evolved()
-        ._then.shouldBeAlive(cell)
+      new GameContext {
+        _with.deadCellAndItsNeighbours(cell, neighbours)
+        _when.evolved()
+        _then.shouldBeAlive(cell)
+      }
     }
   }
 
@@ -155,17 +152,18 @@ class GameOfLifeTest extends AnyFlatSpec with Matchers with TableDrivenPropertyC
         4 to 8: _*
       )
     ) { neighbourCount =>
-      forAll(neighbourCombinationsTable(cell, neighbourCount)) { neighbours =>
-        _givenNewGame
-          ._with.liveCellAndItsNeighbours(cell, neighbours)
-          ._when.evolved()
-          ._then.shouldBeDead(cell)
+      new GameContext {
+        forAll(neighbourCombinationsTable(cell, neighbourCount)) { neighbours =>
+          _with.liveCellAndItsNeighbours(cell, neighbours)
+          _when.evolved()
+          _then.shouldBeDead(cell)
+        }
       }
     }
   }
 
-  "A Penta-decathlon" should "have a period of 15" in {
-    val pentaDecathlonSeed = Seq(
+  "A Penta-decathlon" should "have a period of 15" in new GameContext {
+    private val pentaDecathlonSeed = Seq(
       (1, 1), (1, 2), (1, 3),
       (2, 1), /*   */ (2, 3),
       (3, 1), (3, 2), (3, 3),
@@ -175,24 +173,22 @@ class GameOfLifeTest extends AnyFlatSpec with Matchers with TableDrivenPropertyC
       (7, 1), /*   */ (7, 3),
       (8, 1), (8, 2), (8, 3),
     )
-    _givenNewGame
-      ._with.liveCells(pentaDecathlonSeed: _*)
-      ._then.evolutionShouldNotBePeriodicAtLeastUntil(15)
-      ._thenWhen.evolved()
-      ._then.onlyLiveCellsShouldBe(pentaDecathlonSeed: _*)
+    _with.liveCells(pentaDecathlonSeed: _*)
+    _then.evolutionShouldNotBePeriodicAtLeastUntil(15)
+    _thenWhen.evolved()
+    _then.onlyLiveCellsShouldBe(pentaDecathlonSeed: _*)
   }
 
-  "A Diehard" should "disappear after 130" in {
-    val diehardSeed = Seq(
+  "A Diehard" should "disappear after 130" in new GameContext {
+    private val diehardSeed = Seq(
       (1, 7),
       (2, 1), (2, 2),
       (3, 2), (3, 6), (3, 7), (3, 8),
     )
-    _givenNewGame
-      ._with.liveCells(diehardSeed: _*)
-      ._then.evolutionShouldNotBePeriodicAtLeastUntil(130)
-      ._thenWhen.evolved()
-      ._then.allCellsShouldBeDead
+    _with.liveCells(diehardSeed: _*)
+    _then.evolutionShouldNotBePeriodicAtLeastUntil(130)
+    _thenWhen.evolved()
+    _then.allCellsShouldBeDead
   }
 
   private def neighbourCombinationsTable(cell: (Int, Int), neighbourCount: Int) = {
@@ -220,103 +216,90 @@ class GameOfLifeTest extends AnyFlatSpec with Matchers with TableDrivenPropertyC
 
   }
 
-  private def _givenNewGame: Fixture = {
-    Fixture()
-  }
+  class GameContext:
+    val _with: Builder = Builder()
+    val _when: Whens = Whens()
+    val _then: Thens = Thens()
+    val _thenWhen: Whens = _when
 
-  private class Fixture:
     val game: GameOfLife = GameOfLife()
 
-    var before: Set[(Int, Int)] = Set()
+    private var evolutionTrace: List[Set[(Int, Int)]] = List()
 
-    var evolutionTrace: List[Set[(Int, Int)]] = List()
+    class Builder:
+      def liveCells(cells: (Int, Int)*): Builder =
+        game.spawn(cells)
+        this
 
-    def _with: Fixture = this
+      def deadCells(cells: (Int, Int)*): Builder =
+        game.kill(cells)
+        this
 
-    def _when: Whens = Whens(this)
+      def liveCellAndItsNeighbours(cell: (Int, Int), neighbours: Seq[(Int, Int)]): Builder =
+        game.spawn(Seq(cell))
+        game.spawn(neighbours)
+        this
 
-    def _then: Thens = Thens(this)
+      def deadCellAndItsNeighbours(cell: (Int, Int), neighbours: Seq[(Int, Int)]): Builder =
+        game.kill(Seq(cell))
+        game.spawn(neighbours)
+        this
 
-    def liveCells(cells: (Int, Int)*): Fixture =
-      game.spawn(cells)
-      this
+    class Whens:
+      def cellsSpawned(tuples: (Int, Int)*): Whens =
+        game.spawn(tuples)
+        this
 
-    def deadCells(cells: (Int, Int)*): Fixture =
-      game.kill(cells)
-      this
+      def cellsKilled(tuples: (Int, Int)*): Whens =
+        game.kill(tuples)
+        this
 
-    def liveCellAndItsNeighbours(cell: (Int, Int), neighbours: Seq[(Int, Int)]): Fixture =
-      game.spawn(Seq(cell))
-      game.spawn(neighbours)
-      this
+      def cellsSpawned(json: String): Whens =
+        game.spawn(json)
+        this
 
-    def deadCellAndItsNeighbours(cell: (Int, Int), neighbours: Seq[(Int, Int)]): Fixture =
-      game.kill(Seq(cell))
-      game.spawn(neighbours)
-      this
+      def evolved(): Whens =
+        game.evolve()
+        evolutionTrace = game.liveCells +: evolutionTrace
+        this
 
-  private class Whens(val fixture: Fixture):
-    def _then: Thens = Thens(fixture)
+    class Thens:
+      def onlyLiveCellsShouldBe(expectedLiveCells: (Int, Int)*): Thens =
+        game.liveCells shouldBe expectedLiveCells.toSet
+        this
 
-    def cellsSpawned(tuples: (Int, Int)*): Whens =
-      fixture.game.spawn(tuples)
-      this
+      def allCellsShouldBeDead: Thens =
+        game.liveCells shouldBe empty
+        this
 
-    def cellsKilled(tuples: (Int, Int)*): Whens =
-      fixture.game.kill(tuples)
-      this
+      def shouldBeDead(cells: (Int, Int)*): Thens =
+        cells.foreach(c =>
+          game.liveCells should not contain c
+        )
+        this
 
-    def cellsSpawned(json: String): Whens =
-      fixture.game.spawn(json)
-      this
+      def shouldBeAlive(cells: (Int, Int)*): Thens =
+        cells.foreach(c =>
+          game.liveCells should contain(c)
+        )
+        this
 
-    def evolved(): Whens =
-      fixture.game.evolve()
-      fixture.evolutionTrace = fixture.game.liveCells +: fixture.evolutionTrace
-      this
+      private def shouldBeAlive: Thens =
+        game.liveCells should not be empty
+        this
 
-  private class Thens(val fixture: Fixture):
-    def _then: Thens = this
+      private def shouldBeUnique: Thens =
+        evolutionTrace.tail should not contain game.liveCells
+        this
 
-    def _thenWhen: Whens = fixture._when
-
-    def onlyLiveCellsShouldBe(expectedLiveCells: (Int, Int)*): Thens =
-      fixture.game.liveCells shouldBe expectedLiveCells.toSet
-      this
-
-    def allCellsShouldBeDead: Thens =
-      fixture.game.liveCells shouldBe empty
-      this
-
-    def shouldBeDead(cells: (Int, Int)*): Thens =
-      cells.foreach(c =>
-        fixture.game.liveCells should not contain c
-      )
-      this
-
-    def shouldBeAlive(cells: (Int, Int)*): Thens =
-      cells.foreach(c =>
-        fixture.game.liveCells should contain(c)
-      )
-      this
-
-    def shouldBeAlive: Thens =
-      fixture.game.liveCells should not be empty
-      this
-
-    def shouldBeUnique: Thens =
-      fixture.evolutionTrace.tail should not contain fixture.game.liveCells
-      this
-
-    def evolutionShouldNotBePeriodicAtLeastUntil(period: Int): Thens = {
-      fixture.evolutionTrace = List(fixture.game.liveCells)
-      (1 until period).foreach { i =>
-        fixture._when.evolved()
-          ._then
-          .shouldBeAlive
-          .shouldBeUnique
-      }
-      this
-    }
+      def evolutionShouldNotBePeriodicAtLeastUntil(period: Int): Thens =
+        evolutionTrace = List(game.liveCells)
+        (1 until period).foreach { i =>
+          _when.evolved()
+          _then
+            .shouldBeAlive
+            .shouldBeUnique
+        }
+        this
 
 }
